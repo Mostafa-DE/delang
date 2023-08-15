195dbd44ecdde4b31f839ed7219204f760ca2cb3
mostafa-de <mostafafayyado1@gmail.com>
2023-08-15 23:22:15 +0300

    * Fix env crashing, and get the env directly from railway.

--- src/main.go
+++ src/main.go
@@ -1,28 +1,15 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/Mostafa-DE/delang/execFile"
 	"github.com/Mostafa-DE/delang/repl"
 	"github.com/Mostafa-DE/delang/server"
-	"github.com/joho/godotenv"
 )
 
-func getStartedEnv() string {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
-
-	return os.Getenv("MODE")
-}
-
 func main() {
-	mode := getStartedEnv()
-
-	if mode == "server" {
+	if env := os.Getenv("ENV"); env == "PROD" {
 		server.StartServer()
 		return
 	}
