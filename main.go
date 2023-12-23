8ed7fdb045cf4dcab94c2b205d2a1864a462a717
mostafa-de <mostafafayyado1@gmail.com>
2023-12-23 20:00:56 +0300

    * Add version flag support into the REPL.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"strings"
 
 	"github.com/Mostafa-DE/delang/execFile"
 	"github.com/Mostafa-DE/delang/repl"
@@ -11,6 +12,25 @@ func main() {
 	if len(os.Args) == 1 {
 		repl.StartSession()
 	} else {
+		pathArr := strings.Split(os.Args[1], "/")
+
+		if pathArr[len(pathArr)-1] == "--version" {
+			println("DE v0.0.9")
+			println(DE)
+			return
+		}
+
 		execFile.Run()
 	}
 }
+
+const DE = `
+  ____       ________
+ |  _ \     /|_______|
+ | | | |   | |
+ | | | |   | |_______
+ | | | |   | |_______|
+ | | | |   | |
+ | |_| /   | |_______
+ |____/     \|_______|
+ `
