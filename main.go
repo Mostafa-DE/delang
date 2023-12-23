c7e40533b0556b41dd8aea334e3fae276b6d04ed
mostafa-de <mostafafayyado1@gmail.com>
2023-12-23 18:13:10 +0300

    * Clean up server functionality.

--- main.go
+++ main.go
@@ -5,18 +5,11 @@ import (
 
 	"github.com/Mostafa-DE/delang/execFile"
 	"github.com/Mostafa-DE/delang/repl"
-	"github.com/Mostafa-DE/delang/server"
 )
 
 func main() {
-	if env := os.Getenv("ENV"); env == "PROD" {
-		server.StartServer()
-		return
-	}
-
 	if len(os.Args) == 1 {
 		repl.StartSession()
-		return
 	} else {
 		execFile.Run()
 	}
