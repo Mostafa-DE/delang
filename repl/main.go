a89ee97667e9ce861e4eb03d26e2d4ab07205aba
mostafa-de <mostafafayyado1@gmail.com>
2024-01-10 23:49:30 +0300

    * Refactor REPL. * Fix REPL cursor bug.

--- repl/main.go
+++ repl/main.go
@@ -28,6 +28,7 @@ func StartSession() {
 	historyIndex := 0
 	currentInput := ""
 	cursorPosition := 0
+	quitCount := 0
 
 	fmt.Print("\n")
 	fmt.Print(PROMPT)
@@ -38,104 +39,42 @@ func StartSession() {
 			log.Fatal(err)
 		}
 
-		if key == keyboard.KeyCtrlC {
-			fmt.Println("\nBye!")
-			break
-
-		} else if key == keyboard.KeyEnter {
-			fmt.Println()
-			if currentInput != "" {
-				history = append(history, currentInput)
-				historyIndex = len(history)
-
-				startExec(currentInput, env)
-
-				currentInput = ""
-				cursorPosition = 0
-				fmt.Print(PROMPT)
+		switch key {
+		case keyboard.KeyCtrlC:
+			quitCount++
+			if quitCount == 2 {
+				fmt.Println("\nBye!")
+				return
+			} else {
+				fmt.Println()
+				fmt.Printf("\033[33m%s\033[0m\n", "Press ctrl + c again to exit.")
 			}
 
-		}
+		case keyboard.KeyEnter:
+			handleEnterKey(&history, &historyIndex, &currentInput, &cursorPosition, env)
 
-		if key == keyboard.KeyArrowUp {
-			if historyIndex > 0 {
-				historyIndex--
-				currentInput = history[historyIndex]
-				cursorPosition = len(currentInput)
-				clearCurrentLine()
-				fmt.Print(PROMPT)
-				fmt.Print(currentInput)
-			}
+		case keyboard.KeyArrowUp, keyboard.KeyArrowDown:
+			handleArrowUpDown(key, &history, &historyIndex, &currentInput, &cursorPosition)
 
-		}
+		case keyboard.KeyArrowLeft, keyboard.KeyArrowRight:
+			handleArrowLeftRight(key, &currentInput, &cursorPosition)
 
-		if key == keyboard.KeyArrowDown {
-			if historyIndex < len(history)-1 {
-				historyIndex++
-				currentInput = history[historyIndex]
-				cursorPosition = len(currentInput)
-				clearCurrentLine()
-				fmt.Print(PROMPT)
-				fmt.Print(currentInput)
+		case keyboard.KeyBackspace, keyboard.KeyBackspace2:
+			handleBackspace(&currentInput, &cursorPosition)
 
-			} else if historyIndex == len(history)-1 {
-				historyIndex++
-				currentInput = ""
-				cursorPosition = 0
-				clearCurrentLine()
-				fmt.Print(PROMPT)
+		case keyboard.KeySpace:
+			insertCharacterAtCursor(&currentInput, &cursorPosition, " ")
+
+		case keyboard.KeyCtrlL:
+			clearScreen()
+
+		default:
+			if char != 0 {
+				insertCharacterAtCursor(&currentInput, &cursorPosition, string(char))
 			}
 		}
 
-		if key == keyboard.KeyBackspace || key == keyboard.KeyBackspace2 {
-			if cursorPosition > 0 {
-				currentInput = currentInput[:cursorPosition-1] + currentInput[cursorPosition:]
-				cursorPosition--
-				clearCurrentLine()
-				fmt.Print(PROMPT)
-				fmt.Print(currentInput)
-				moveCursorLeft(len(currentInput) - cursorPosition - 1)
-			}
-		}
-
-		if key == keyboard.KeyArrowLeft {
-			if cursorPosition > 0 {
-				cursorPosition--
-				moveCursorLeft(1)
-			}
-		}
-
-		if key == keyboard.KeyArrowRight {
-			if cursorPosition < len(currentInput) {
-				cursorPosition++
-				moveCursorRight(1)
-			}
-		}
-
-		if key == keyboard.KeyCtrlL {
-			fmt.Print("\033[H\033[2J")
-			fmt.Print(PROMPT)
-		}
-
-		if char != 0 {
-			currentInput = currentInput[:cursorPosition] + string(char) + currentInput[cursorPosition:]
-			cursorPosition++
-			clearCurrentLine()
-			fmt.Print(PROMPT)
-			fmt.Print(currentInput)
-			moveCursorLeft(len(currentInput) - cursorPosition - 1)
-		}
-
-		if key == keyboard.KeySpace {
-			currentInput = currentInput[:cursorPosition] + " " + currentInput[cursorPosition:]
-			cursorPosition++
-			clearCurrentLine()
-			fmt.Print(PROMPT)
-			fmt.Print(currentInput)
-
-			// TODO: Fix the cursor position after adding a space
-			moveCursorLeft(len(currentInput) - cursorPosition - 1)
-		}
+		refreshLine(PROMPT, currentInput, cursorPosition)
 	}
 }
 
@@ -173,5 +112,84 @@ func startExec(command string, env *object.Environment) {
 	} else {
 		fmt.Println("null")
 	}
+}
 
+func handleEnterKey(history *[]string, historyIndex *int, currentInput *string, cursorPosition *int, env *object.Environment) {
+	fmt.Println()
+	if *currentInput != "" {
+		*history = append(*history, *currentInput)
+		*historyIndex = len(*history)
+
+		startExec(*currentInput, env)
+
+		*currentInput = ""
+		*cursorPosition = 0
+	}
+}
+
+func handleArrowUpDown(key keyboard.Key, history *[]string, historyIndex *int, currentInput *string, cursorPosition *int) {
+	if key == keyboard.KeyArrowUp && *historyIndex > 0 { // we check if historyIndex > 0 to prevent index out of range error
+		*historyIndex--
+	} else if key == keyboard.KeyArrowDown && *historyIndex < len(*history)-1 {
+		*historyIndex++
+	}
+
+	if key == keyboard.KeyArrowUp || (key == keyboard.KeyArrowDown && *historyIndex < len(*history)) {
+		*currentInput = (*history)[*historyIndex]
+		*cursorPosition = len(*currentInput)
+	} else if key == keyboard.KeyArrowDown && *historyIndex == len(*history) {
+		// This is to handle the case when the user presses the down arrow key after reaching the end of the history
+		*currentInput = ""
+		*cursorPosition = 0
+	}
+}
+
+func handleArrowLeftRight(key keyboard.Key, currentInput *string, cursorPosition *int) {
+	if key == keyboard.KeyArrowLeft && *cursorPosition > 0 {
+		*cursorPosition--
+	} else if key == keyboard.KeyArrowRight && *cursorPosition < len(*currentInput) {
+		*cursorPosition++
+	}
+}
+
+func handleBackspace(currentInput *string, cursorPosition *int) {
+	if *cursorPosition > 0 {
+		/*
+			** We use `cursorPosition-1` because we want to delete the character before the cursor
+			and then move the cursor back one position.
+
+			==> e.g. if the cursor is at position 5, we want to delete the character at position 4
+				and then move the cursor back to position 4.
+
+			** Not only this but also we want to keep anything after the cursor position.
+
+			==> e.g. if the currentInput is "hello world" and the cursor is at position 5,
+				we want to delete the character at position 4 and then move the cursor back to position 4
+				and keep the rest of the string after the cursor position. so the result will be "hell world".
+		*/
+
+		*currentInput = (*currentInput)[:*cursorPosition-1] + (*currentInput)[*cursorPosition:]
+		*cursorPosition--
+	}
+}
+
+func insertCharacterAtCursor(currentInput *string, cursorPosition *int, char string) {
+	// This function inserts a character at the cursor position and then moves the cursor forward one position.
+	*currentInput = (*currentInput)[:*cursorPosition] + char + (*currentInput)[*cursorPosition:]
+	*cursorPosition++
+}
+
+func clearScreen() {
+	fmt.Print("\033[H\033[2J")
+}
+
+func refreshLine(prompt string, currentInput string, cursorPosition int) {
+	clearCurrentLine()
+	fmt.Print(prompt)
+	fmt.Print(currentInput)
+
+	// We only need to move the cursor if the cursor is not at the end of the line
+	if len(currentInput) > cursorPosition {
+		moveCursorLeft(len(currentInput) - cursorPosition)
+	}
 }
