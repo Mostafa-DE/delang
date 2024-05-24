a37aa8b5b1144c2fcd01b1c680e776c19fbebff5
mostafa-de <mostafafayyado1@gmail.com>
2024-05-24 22:45:19 +0300

    Remove DE version number in REPL

--- repl/main.go
+++ repl/main.go
@@ -22,7 +22,7 @@ func StartSession() {
 	}
 	defer keyboard.Close()
 
-	fmt.Printf("Hi! Welcome to DE v0.0.11\n")
+	fmt.Printf("Hi! Welcome to DE\n")
 	fmt.Printf("Type '.help' to see a list of commands.\n")
 
 	history := []string{}
